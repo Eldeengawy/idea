039fc88c30681c997a559b6b8aef0ba161d64723
Mahmoud Eldengawy <eldengaawy@gmail.com>
2023-07-24 19:05:10 +0300

    mode switch button

---
+++ lib/views/widgets/custom_mode_switch_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class CustomModeSwitchButton extends StatelessWidget {
+  final bool isNightMode;
+  final VoidCallback onTap;
+
+  const CustomModeSwitchButton({
+    Key? key,
+    required this.isNightMode,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(10.0),
+          color: isNightMode
+              ? Colors.blueGrey.withOpacity(0.6)
+              : Colors.yellow.withOpacity(0.3),
+        ),
+        child: Center(
+          child: Icon(
+            isNightMode ? FeatherIcons.moon : FeatherIcons.sun,
+            color: Colors.white,
+            size: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
