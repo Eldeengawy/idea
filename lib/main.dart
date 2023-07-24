ecd2db08ce45879254328cc7831dbf63d98daaaf
Mahmoud Eldengawy <eldengaawy@gmail.com>
2023-07-24 19:04:36 +0300

    status bar and app theme in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:idea/app_constants.dart';
 import 'package:idea/views/notes_view.dart';
 
 void main() {
@@ -6,18 +8,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Set the status bar color to dark background with white (light) text
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent, // For transparent status bar
+      statusBarIconBrightness: Brightness.light,
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Idea',
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          useMaterial3: true,
-          scaffoldBackgroundColor: const Color(0xff0c0a0b)),
+      theme: isNightMode
+          ? AppThemes.darkTheme
+          : AppThemes.lightTheme, // Set the theme based on isNightMode flag
+
+      // theme: ThemeData(
+      //     brightness: Brightness.dark,
+      //     useMaterial3: true,
+      //     scaffoldBackgroundColor: const Color(0xff0c0a0b)),
       home: const NotesView(),
     );
   }
