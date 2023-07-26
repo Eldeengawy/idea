4945df5a502385dd517011fef08d420b6b964457
Mahmoud Eldengawy <eldengaawy@gmail.com>
2023-07-26 23:11:00 +0300

    generate app images

---
+++ lib/constants/app_images.dart
@@ -0,0 +1,7 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesProfilePic1
+  /// assets/images/profile_pic1.png
+  static const String imagesProfilePic1 = "assets/images/profile_pic1.png";
+}
