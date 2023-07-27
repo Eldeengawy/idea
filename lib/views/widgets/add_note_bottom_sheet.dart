312b17def2b4f899b9390a71a6539300cb2c90de
Mahmoud Eldengawy <eldengaawy@gmail.com>
2023-07-27 11:21:18 +0300

    add note bottom sheet loading

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:idea/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:idea/views/widgets/add_note_form.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   final void Function(
@@ -16,26 +15,24 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (BuildContext context) => AddNoteCubit(),
-      child: SizedBox(
-        height: 600, // Specify your desired fixed height here
-        child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteFailure) {
-              if (kDebugMode) {
-                print('Failed ${state.errMessage}');
-              }
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteFailure) {
+            if (kDebugMode) {
+              print('Failed ${state.errMessage}');
             }
+          }
 
-            if (state is AddNoteSuccess) {
-              Navigator.pop(context);
-            }
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-                inAsyncCall: state is AddNoteLoading ? true : false,
-                child: AddNoteForm(onAddFolder: onAddFolder));
-          },
-        ),
+          if (state is AddNoteSuccess) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return AbsorbPointer(
+            absorbing: state is AddNoteLoading ? true : false,
+            child: AddNoteForm(onAddFolder: onAddFolder),
+          );
+        },
       ),
     );
   }
--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -131,31 +131,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Expanded(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(vertical: 18.0),
-                          backgroundColor: const Color(0xffff9e37),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          elevation: 4,
-                        ),
-                        onPressed: _submitForm,
-                        child: const Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.add, size: 24),
-                            SizedBox(width: 8),
-                            Text(
-                              'Add Note',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
+                        child: BlocBuilder<AddNoteCubit, AddNoteState>(
+                      builder: (BuildContext context, AddNoteState state) =>
+                          CustomButton(
+                        isLoading: state is AddNoteLoading ? true : false,
+                        onTap: _submitForm,
                       ),
-                    ),
+                    )),
                     const SizedBox(
                       width: 20.0,
                     ),
@@ -171,5 +153,51 @@ class _AddNoteFormState extends State<AddNoteForm> {
         ),
       ],
     );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
+  final void Function()? onTap;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return !isLoading
+        ? ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 18.0),
+              backgroundColor: const Color(0xffff9e37),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              elevation: 4,
+            ),
+            onPressed: onTap,
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.add, size: 24),
+                SizedBox(width: 8),
+                Text(
+                  'Add Note',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          )
+        : const Center(
+            child: SizedBox(
+              height: 24.0,
+              width: 24.0,
+              child: CircularProgressIndicator(
+                strokeCap: StrokeCap.round,
+                // strokeWidth: 6.0,
+
+                color: Colors.amber,
+              ),
+            ),
+          );
   }
 }
